a9a20746c881b6c49137520877680d79f02c9b17
Andrew Chou <andrewchou@fastmail.com>
2022-12-05 12:29:35 -0500

    maybe fix issue in test script

--- scripts/test-react-native-setup.sh
+++ scripts/test-react-native-setup.sh
@@ -2,14 +2,18 @@
 
 set -e
 
-echo "Check Buck setup"
+export KOTLIN_HOME="third-party/kotlin"
+
+echo "Download Buck dependencies"
 ./scripts/circleci/buck_fetch.sh
+
+echo "Build React Native via Buck"
 buck build ReactAndroid/src/main/java/com/facebook/react
 buck build ReactAndroid/src/main/java/com/facebook/react/shell
 
-echo "Build React Native"
+echo "Build React Native via Gradle"
 yarn install
-./gradlew --no-daemon :ReactAndroid:packageReactNdkLibsForBuck
+./gradlew --no-daemon :ReactAndroid:packageReactNdkLibsForBuck -PreactNativeArchitectures=arm64-v8a
 
-echo "Assemble RNTester app"
-./gradlew --no-daemon :packages:rn-tester:android:app:assembleRelease
+echo "Assemble RNTester Release arm64-v8a"
+./gradlew --no-daemon :packages:rn-tester:android:app:assembleRelease -PreactNativeArchitectures=arm64-v8a
